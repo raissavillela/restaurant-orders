588cb82dcda94b890bddac1c58c1a766b20c2ef1
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-12 19:08:33 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+
+    ingredient = Ingredient("creme de leite")
+    assert isinstance(ingredient, Ingredient)
+    assert ingredient.name == "creme de leite"
+
+    expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.restrictions == expected_restrictions
+
+    assert repr(ingredient) == "Ingredient('creme de leite)"
+
+    same_ingredient = Ingredient("creme de leite")
+    different_ingredient = Ingredient("carne")
+    assert ingredient == same_ingredient
+    assert ingredient != different_ingredient
+
+    assert hash(ingredient) == hash(same_ingredient)
+    assert hash(ingredient) != hash(different_ingredient)
